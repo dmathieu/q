b7d183211ce54150ab52901429f5888e5d461a26
dmathieu <42@dmathieu.com>
2017-06-28 15:18:06 +0200

    run a single for loop

--- run.go
+++ run.go
@@ -10,30 +10,15 @@ func (q *Queue) Run(handler func([]byte) error, mc int) error {
 	c := make(chan struct{}, mc)
 	errCh := make(chan error)
 
-	go func() {
-		defer func() {
-			if x := recover(); x != nil {
-				err, ok := x.(error)
-				if !ok {
-					err = fmt.Errorf("%q", err)
-				}
-				errCh <- err
-			}
-		}()
-
-		for {
-			select {
-			case <-time.After(time.Minute):
+	for {
+		select {
+		case <-time.After(time.Minute):
+			go func() {
 				err := q.HouseKeeping()
 				if err != nil {
 					errCh <- err
 				}
-			}
-		}
-	}()
-
-	for {
-		select {
+			}()
 		case err := <-errCh:
 			return err
 		case c <- struct{}{}:
