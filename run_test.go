02dd6fbf97dab4251be974bc00b1cb1100af29bd
dmathieu <42@dmathieu.com>
2017-06-30 14:29:49 +0200

    add test checking Run with an error behaves properly

--- run_test.go
+++ run_test.go
@@ -1,6 +1,7 @@
 package q
 
 import (
+	"errors"
 	"sync"
 	"testing"
 	"time"
@@ -22,6 +23,29 @@ func TestRun(t *testing.T) {
 
 				received = append(received, d)
 				return nil
+			}, 1)
+			assert.Nil(t, err)
+		}()
+
+		q.Enqueue([]byte("hello"))
+		q.Enqueue([]byte("world"))
+
+		time.Sleep(time.Millisecond)
+		mutex.Lock()
+		assert.Equal(t, [][]byte{[]byte("world"), []byte("hello")}, received)
+		mutex.Unlock()
+	})
+
+	t.Run("with an error", func(t *testing.T) {
+		var mutex = &sync.Mutex{}
+		var received [][]byte
+		go func() {
+			err := q.Run(func(d []byte) error {
+				mutex.Lock()
+				defer mutex.Unlock()
+
+				received = append(received, d)
+				return errors.New("an error occured")
 			}, 1)
 			assert.Nil(t, err)
 		}()
